6ebf2193467d71b94d80d145d2fed937ff1b1c93
huangjipeng <1319639755@qq.com>
2022-01-17 12:38:38 +0800

    test: add tjs file lint-staged test jsx

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,63 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import qs from 'qs'
+import qs from "qs";
 
-import { cleanObject } from '../../utils'
+import { cleanObject } from "../../utils";
 
-import SearchPanel from './search-panel'
-import List from './list'
+import SearchPanel from "./search-panel";
+import List from "./list";
 
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const ProjectListScreen = () => {
-
-  const [ param, setParam ] = useState({
-    name: '',
-    personId: ''
-  })
-  const [ users, setUsers ] = useState([])
-  const [ list, setList ] = useState([])
-
-  useEffect(() => {
-    fetch(`${ apiUrl }/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
-  }, [param])
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch(`${ apiUrl }/users`).then(async response => {
-      if (response.ok) {
-        setUsers(await response.json())
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
       }
-    })
+    );
+  }, [param]);
 
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
 
+  return (
+    <div className="test">
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
 
-
-    
-  }, [])
-
-  return <div>
-    <SearchPanel param={ param } setParam={ setParam } users={ users } />
-    <List users={ users } list={ list } />
-  </div>
-}
-
-
-
-
-
-export default ProjectListScreen
-
-
-
-
-
-
-
-
-
-
-
+export default ProjectListScreen;
