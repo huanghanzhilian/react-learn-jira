46caa356a8a5f3385f3abc1ef17dea0c54211a9e
huangjipeng <1319639755@qq.com>
2022-01-18 16:46:51 +0800

    feat: custem useAuth and use context

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,33 +1,26 @@
 import { FormEvent } from "react";
 
+import { useAuth } from "context/auth-context";
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(param),
-    }).then(async (response) => {
-      console.log(response);
-    });
-  };
+  const { login, user } = useAuth();
+
+  console.log(user);
 
   const handleSubimit = (event: FormEvent<HTMLFormElement>) => {
-    console.log(event.currentTarget);
     const username = (event.currentTarget.elements[0] as HTMLInputElement)
       .value;
     const password = (event.currentTarget.elements[1] as HTMLInputElement)
       .value;
-    console.log(username);
-    console.log(password);
     login({ username, password });
     event.preventDefault();
   };
   return (
     <form onSubmit={handleSubimit}>
+      {user ? <div>登陆成功，用户名：{user?.name}</div> : null}
+
       <div>
         <label htmlFor="usename">用户名</label>
         <input type="text" id="username" />
