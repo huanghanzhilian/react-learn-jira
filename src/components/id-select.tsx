532f04865d6f6cd9b8770d9fbe56440fa2438491
huangjipeng <1319639755@qq.com>
2022-01-25 12:38:00 +0800

    feat: add id-select.tsx

---
+++ src/components/id-select.tsx
@@ -0,0 +1,39 @@
+import { Select } from "antd";
+import { Raw } from "types";
+
+interface IdSelectProps {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * value 可以传入多种类型的值
+ * onChange 只会回调 number | undefined 类型
+ * 当 isNan(Number(value)) 为true的时候， 代表选择默认类型
+ * 当选择默认类型的时候，onChange会回调undefined
+ * @param props
+ */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options } = props;
+
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+// 转换为数字 不是数字转换为0
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
