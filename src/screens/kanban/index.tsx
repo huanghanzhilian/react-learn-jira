337f24cdeefe78d665fef7f2e0d25192cdf5b54e
huangjipeng <1319639755@qq.com>
2022-02-18 16:23:53 +0800

    feat: 初步完成看板

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,7 +1,31 @@
-import { useUrlQueryParam } from "utils/url";
+import styled from "@emotion/styled";
+import { Kanban } from "types/kanban";
+import { useDocumentTitle } from "utils";
+import { useKanbans } from "utils/kanban";
+import { KanbanColumn } from "./kanban-column";
+import { useKanbanSearchParams, useProjectInUrl } from "./util";
 
 const KanbanScreen = () => {
-  return <div>KanbanScreen</div>;
+  useDocumentTitle("看板列表");
+
+  const { data: currentProject } = useProjectInUrl();
+  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  return (
+    <div>
+      <h1>{currentProject?.name}看板</h1>
+      <ColumnsContainer>
+        {kanbans?.map((kanban: Kanban) => (
+          <KanbanColumn kanban={kanban} key={kanban.id} />
+        ))}
+      </ColumnsContainer>
+    </div>
+  );
 };
+
+const ColumnsContainer = styled.div`
+  display: flex;
+  overflow: hidden;
+  margin-right: 2rem;
+`;
 
 export default KanbanScreen;
